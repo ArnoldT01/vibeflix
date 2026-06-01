68e0c14f246777c0bd34bffd8183833c08fa37a4
Arnold <arnoldtee01@gmail.com>
2026-06-01 21:41:19 +0200

    remove hero background image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,7 +21,6 @@ const HomePage = () => {
 
     return (
         <main>
-            <div className="pattern" />
             <div className="wrapper">
                 <header>
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
